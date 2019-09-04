bcbaaa7757375fdbfd9e8e1dc518f92fe04d2664
Ľubomír Majerčík <45042522+meiercheek@users.noreply.github.com>
2019-09-04 22:34:25 +0200

    commit

---
+++ scripts/web/locale.js
@@ -0,0 +1,90 @@
+function locale(){
+    let lg = "en"; 
+    //document.getElementById('info').innerHTML = lang.lg.intro;
+    alert(lang.lg.intro);
+}
+
+locale();
+const lang = {
+    "en":{
+        intro: "computer science student, web developer and aspiring video game programmer",
+        li:"add me on linkedin",
+        cv:"look at my cv",
+        sgDesc:"side-scrolling platformer made in js inspired by super mario bros.",
+        try:"try it out",
+        skills:"my skills",
+        back:"go back",
+        skill:"skill",
+        extent:"extent",
+        langs:"languages i speak",
+        beginner:"beginner",
+        intermediate:"intermediate",
+        advanced:"advanced",
+
+    },
+    
+    sk:{
+        intro: "študent informatiky, web developer a ašpirujúci vývojár videohier",
+        li:"pridaj si ma na linkedin-e",
+        cv:"pozri si moje cv",
+        sgDesc:"side-scrolling platformovka vyrobená v js a inšpirovaná super mario bros.",
+        try:"vyskúšaj",
+        skills:"moje zručnosti",
+        back:"späť",
+        skill:"zručnosť",
+        extent:"rozsah",
+        langs:"jazyky, ktorými hovorím",
+        beginner:"začiatočník",
+        intermediate:"pokročilý",
+        advanced:"expert",      
+    },
+
+    de:{
+        intro: "informatikstudent, webentwickler und aufstrebender videospielentwickler",
+        li:"füge mich auf linkedin hinzu",
+        cv:"schau dir meinen lebenslauf an",
+        sgDesc:"side-scrolling platformer entwickelt in js, inspiriert von super mario bros.",
+        try:"probier es aus",
+        skills:"meine fähigkeiten",
+        back:"geh zurück",
+        skill:"fähigkeit",
+        extent:"umfang",
+        langs:"die sprachen ich spreche",
+        beginner:"anfänger",
+        intermediate:"mittlere",
+        advanced:"fortgeschritten",       
+    },
+
+    ru:{
+        intro: "студент информатики, веб-разработчик и начинающий разработчик видеоигр",
+        li:"добавь меня в linkedin",
+        cv:"посмотри на мое резюме",
+        sgDesc:"платформер боковой прокрутки, сделанный в js, вдохновленный super mario bros.",
+        try:"посмотри",
+        skills:"мои навыки",
+        back:"вернитесь",
+        skill:"умение",
+        extent:"степень",
+        langs:"я говорю",
+        beginner:"начинающий",
+        intermediate:"промежуточный",
+        advanced:"передовой",       
+    },
+
+    pl:{
+        intro: "student informatyki, web developer i początkujący programista gier wideo",
+        li:"dodaj mnie na linkedin",
+        cv:"spójrz na moje cv",
+        sgDesc:"side-scrolling platformówka wykonana w js inspirowana super mario bros.",
+        try:"spróbuj",
+        skills:"moje umiejętności",
+        back:"wróć",
+        skill:"umiejętność",
+        extent:"stopień",
+        langs:"języki, którymi mówię",
+        beginner:"początkujący",
+        intermediate:"pośredni",
+        advanced:"zaawansowany",      
+    }
+}
+
